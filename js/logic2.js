afe96c9f0e69a986e4b7b0dd39e92d9c9817f988
jlmendoza <joseluismendozal10@gmail.com>
2019-10-05 12:15:15 -0500

    JL

--- js/logic2.js
+++ js/logic2.js
@@ -4,7 +4,7 @@
  */
 
 // Center of the map with these to variables
-var mexicoCity = [24.0277, -101.900];
+var mexicoCity = [24.447417, -101.99313];
 var zoomLevel = 5;
 
 
@@ -64,6 +64,16 @@ var positron = L.tileLayer(
         attribution: "©OpenStreetMap, ©CartoDB"
     }
 ).addTo(map);
+
+L.control.fullscreen({
+    position: 'topleft', // change the position of the button can be topleft, topright, bottomright or bottomleft, defaut topleft
+    title: 'Show me the fullscreen !', // change the title of the button, default Full Screen
+    titleCancel: 'Exit fullscreen mode', // change the title of the button when fullscreen is on, default Exit Full Screen
+    content: null, // change the content of the button, can be HTML, default null
+    forceSeparateButton: true, // force seperate button to detach from zoom buttons, default false
+    forcePseudoFullscreen: true, // force use of pseudo full screen even if full screen API is available, default false
+    fullscreenElement: false // Dom element to render in full screen, false by default, fallback to map._container
+  }).addTo(map);
 
 // First Style
 
@@ -135,6 +145,7 @@ function onEachFeature(feature, layer) {
         mouseover: highlightFeature,
         mouseout: resetHighlight,
         click: zoomToFeature
+        
     });
 }
 
