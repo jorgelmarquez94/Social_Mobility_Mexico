c5a048e4dcfefda31c5b6b3719d8773ac16031a3
jorgelmarquez94 <jorgelmarquez94@gmail.com>
2019-10-08 00:30:26 -0500

    Sankey Diagram Version 1.1

--- app.py
+++ app.py
@@ -27,18 +27,44 @@ engine = create_engine("sqlite:///MMSI_2016.sqlite")
 #################################################
 # Flask Routes
 #################################################
+@app.route("/getMyJson")
+def getMyJson():
+    """Return a list of specific demographic data"""
+    
+    results = engine.execute("SELECT Variable,perspectiva_se_14_anios,opinion_situacion_economica,factor_expansion,Node_color,Link_color FROM MMSI_2016_Demográficos Where demografico = 'Sexo'")
+
+    # Create a dictionary entry for each row of metadata information
+    df = DataFrame(results.fetchall())
+    df.columns = results.keys()
+    df = df.to_json(orient='columns')
+
+    print(df)
+    return(df)
+
 
 @app.route("/gender")
 def index():
     #Return the homepage.
     return render_template("gender.html")
 
+def load_json():
+    """Return a list of specific demographic data"""
+    
+    results = engine.execute("SELECT Variable,perspectiva_se_14_anios,opinion_situacion_economica,factor_expansion,Node_color,Link_color FROM MMSI_2016_Demográficos Where demografico = 'Sexo'")
+
+    # Create a dictionary entry for each row of metadata information
+    df = DataFrame(results.fetchall())
+    df.columns = results.keys()
+    df = df.to_json()
+
+    print(df)
+    return jsonify(df)
 
 @app.route("/gender")
 def load_json():
     """Return a list of specific demographic data"""
     
-    results = engine.execute("SELECT Variable,Perspectiva_SE_14_años,Opinión_Situación_Económica,Factor_Expansión,Node_color,Link_color FROM MMSI_2016_Demográficos Where Demográfico = 'Sexo'")
+    results = engine.execute("SELECT Variable,perspectiva_se_14_anios,opinion_situacion_economica,factor_expansion,Node_color,Link_color FROM MMSI_2016_Demográficos Where demografico = 'Sexo'")
 
     # Create a dictionary entry for each row of metadata information
     df = DataFrame(results.fetchall())
@@ -52,6 +78,7 @@ def load_json():
 def map():
     #Return the maps page.
     return render_template("maps.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,76 +1,79 @@
 //------- Individual Sample Metadata Info and Gauge Chart
-function init(){
-  var url_gender=`/gender`;
-            Plotly.d3.json(url_gender, function(fig){
-              console.log("total: "+Object.keys(fig.Perspectiva_SE_14_a\u00f1os).length);
-              
-              var sources = []
-              for (var i = 0; i < Object.keys(fig.Perspectiva_SE_14_a\u00f1os).length; i++) {
-                sources.push(fig.Perspectiva_SE_14_a\u00f1os[i]);
-              }
-            ​
-              var targets = []
-              for (var i = 0; i < Object.keys(fig.Opini\u00f3n_Situaci\u00f3n_Econ\u00f3mica).length; i++) {
-                targets.push(10+fig.Opini\u00f3n_Situaci\u00f3n_Econ\u00f3mica[i]);
-              }
-            ​
-              var values = []
-              
-              for (var i = 0; i < Object.keys(fig.Factor_Expansi\u00f3n).length; i++) {
-                values.push(fig.Factor_Expansi\u00f3n[i]);
-              }
-            ​
-              var colors = ["","rgb(125, 0, 0)","rgb(255, 0, 0)","rgb(255, 125, 0)","rgb(255, 255, 0)",
+function loadSankey(){
+  var url_gender=`/getMyJson`;
+  Plotly.d3.json(url_gender, function(fig){
+    console.log("total: "+Object.keys(fig.perspectiva_se_14_anios).length);
+
+    var sources = []
+      for (var i = 0; i < Object.keys(fig.perspectiva_se_14_anios).length; i++) {
+        sources.push(fig.perspectiva_se_14_anios[i]);
+      }
+
+      var targets = []
+      for (var i = 0; i < Object.keys(fig.opinion_situacion_economica).length; i++) {
+        targets.push(10+fig.opinion_situacion_economica[i]);
+      }
+
+      var values = []
+
+      for (var i = 0; i < Object.keys(fig.factor_expansion).length; i++) {
+        values.push(fig.factor_expansion[i]);
+      }
+
+      var colors = ["","rgb(125, 0, 0)","rgb(255, 0, 0)","rgb(255, 125, 0)","rgb(255, 255, 0)",
                            "rgb(255, 255, 0)","rgb(128, 128, 0)","rgb(0, 255, 0)","rgb(0, 128, 0)",
                            "rgb(0, 0, 255)","rgb(0, 0, 128)"]
-            ​
-              var color_path =[]
-              for (var i = 0; i < Object.keys(fig.Link_color).length; i++) {
-                color_path.push(fig.Link_color[i]);
-              }
-            ​
-            ​
-              var data = {
-              type: "sankey",
-              orientation: "h",
-              domain:{
-                x:[0,1],
-                y:[0,1]
-              },
-              node: {
-                pad: 15,
-                thickness: 15,
-                line: {
-                  color: "black",
-                  width: 0.5
-                },
-                //Ruta a documento "data" en formato JSON
-                label: ["",1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-                color: colors,
-                font: 10
-              },
-              link: {
-                 //Ruta a documento "data" en formato JSON
-                source:sources,
-                target:targets,
-                value:values,
-                //Color of the link/path 
-                color: color_path
-              }
-            }
-            ​
-            var data = [data]
-            ​
-            var layout = {
-              title: "Social Mobility",
-              font: {
-                size: 15
-              }
-            }
-            ​
-            Plotly.react('myDiv', data, layout, {showSendToCloud:true})
-            });
 
+      var color_path =[]
+      for (var i = 0; i < Object.keys(fig.Link_color).length; i++) {
+        color_path.push(fig.Link_color[i]);
+      }
+
+      var data = {
+        type: "sankey",
+        orientation: "h",
+        domain:{
+          x:[0,1],
+          y:[0,1]
+        },
+        node: {
+          pad: 15,
+          thickness: 15,
+          line: {
+            color: "black",
+            width: 0.5
+          },
+          //Ruta a documento "data" en formato JSON
+          label: ["",1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
+          color: colors,
+          font: 10
+        },
+        link: {
+        //Ruta a documento "data" en formato JSON
+        source:sources,
+        target:targets,
+        value:values,
+        //Color of the link/path 
+        color: color_path
+        }
+      }
+
+      var data = [data]
+
+      var layout = {
+        title: "Social Mobility",
+        font: {
+          size: 15
+        }
+      }
+
+      Plotly.react('myDiv', data, layout, {showSendToCloud:true})
+    }
+  );
+}
+
+function init() {
+  loadSankey();
 }
 
 // Initialize the dashboard
